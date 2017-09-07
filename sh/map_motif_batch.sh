d488e4dbb634a3eb14ba399614f3373852043b1b
Ciera Martinez <ciera.martinez@gmail.com>
2017-09-07 16:08:19 -0700

    coming back to this project and realize everything needs organizing

--- sh/map_motif_batch.sh
+++ sh/map_motif_batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in ../data/fasta/kvon_alignments/*
+for file in ../data/fasta/kvon_alignments/sorted/*
 do
-  python map_motif.py $file ../data/PWM/fm/todo/kr_FlyReg.fm
+  python map_motif.py $file ../data/PWM/fm/gt_nar2008.fm
 done
